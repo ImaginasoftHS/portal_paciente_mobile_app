1d18f3e58c4085770757f1ce0f2e20973afed72e
Nuno Miranda <nuno_miranda@outlook.com>
2024-05-14 18:25:18 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import * as React from 'react';
+import { AppRegistry,Text,StatusBar,StyleSheet} from 'react-native';
+import { PaperProvider , MD3DarkTheme as DefaultTheme} from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 export default function App() {
+  const Stack = createStackNavigator();
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'tomato',
+      secondary: 'yellow',
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar  />
+
+    </PaperProvider>
+
   );
 }
 
